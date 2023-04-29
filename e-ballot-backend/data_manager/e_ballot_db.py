0f28d9666fb107d9a220a3e61728b15e9f65994c
Yazan Armoush <yazan.armoush1@gmail.com>
2023-04-29 15:53:30 -0400

    Added Party Collection

--- e-ballot-backend/data_manager/e_ballot_db.py
+++ e-ballot-backend/data_manager/e_ballot_db.py
@@ -9,9 +9,9 @@ from data_manager._e_ballot_secrets import get_db
 # Get DB From Cluster
 USEC_DB_ADDRESS, USEC_DB_NAME = get_db()
 _cluster = MongoClient(USEC_DB_ADDRESS)
-_crew_ms_db = _cluster[USEC_DB_NAME]
+_e_ballot_db = _cluster[USEC_DB_NAME]
 
 
 class E_BALLOT_DB:
-    users_coll: Collection = _crew_ms_db.get_collection("users")
+    users_coll: Collection = _e_ballot_db.get_collection("users")
     party_coll: Collection = _e_ballot_db.get_collection("party")
