289b6d76c7f37854f5c2a9d2dd18a4f142f21d4f
Ishav Sohal <ishav.sohal@mail.utoronto.ca>
2023-04-29 15:32:43 -0400

    Created Party class

--- e-ballot-backend/Models/voting.py
+++ e-ballot-backend/Models/voting.py
@@ -5,38 +5,84 @@ from bson.binary import Binary
 from datetime import datetime
 from routes.users.get import get_usersObject
 
-class Voting:
-    
-    def __init__(self) -> None:
-        self.votedUsers = {}
-        self.partyVotes = {}
-        self.allUsers = get_usersObject()
+class Party:
+    oid: ObjectId
+    name: str
+    ontario: list[ObjectId]
+    quebec: list[ObjectId]
+    bc: list[ObjectId]
+    alberta: list[ObjectId]
+
+    def __init__(self, oid, name, ontario, quebec, bc, alberta):
+        self.oid = oid
+        self.name = name
+        self.ontario = ontario
+        self.quebec = quebec
+        self.bc = bc
+        self.alberta = alberta
+
+    @staticmethod
+    def from_json(doc: dict) -> Party:
+        return User.Profile(
+            oid=doc['_id'],
+            name=doc['name'],
+            ontario=doc['ontario'],
+            quebec=doc['quebec'],
+            bc=doc['bc'],
+            alberta=doc['alberta']
+        )
+
+    def to_json(self) -> dict:
+        return \
+            {
+                '_id': self.oid,
+                'name': self.name,
+                'ontario': self.ontario,
+                'quebec': self.quebec,
+                'bc': self.bc,
+                'alberta': self.alberta
+            }
+
+    def get_all_voters(self):
+        return self.ontario + self.quebec + self.bc + self.alberta
+
+
+class VotingSystem:
+
+    votedUsers: dict
+    parties: Party
+
+    def __init__(self, votedUsers, parties) -> None:
+        self.votedUsers = votedUsers
+        self.partyVotes = parties
 
     # Fix: changed User to str, changed user.oid to user
-    def vote(self, user: User, party: str):
-       # step 1: check if the user exists in the database
-       # step 2: check if they already voted or not
-       # step 3: if they have, then return
-       # step 4: if they havent, then decide which party the user wants to vote for and increment the specific party
-       for i in self.allUsers:
-        if str(user.oid) == i["_id"]:
-            if user not in self.votedUsers:
-                self.votedUsers[user] = party
-            else:
-                return
-            if party not in self.partyVotes:
-                self.partyVotes[party] = 1
-            else:
-                self.partyVotes[party] += 1
-    
+    def vote(self, user: User, party: Party):
+        allUsers = get_usersObject()
+
+        # step 1: check if the user exists in the database
+        # step 2: check if they already voted or not
+        # step 3: if they have, then return
+        # step 4: if they havent, then decide which party the user wants to vote for and increment the specific party
+        for i in allUsers:
+            if str(user.oid) == i["_id"]:
+                if user not in self.votedUsers:
+                    self.votedUsers[user] = party
+                else:
+                    return
+                if party not in self.partyVotes:
+                    self.partyVotes[party] = 1
+                else:
+                    self.partyVotes[party] += 1
+
     # utility methods
     def getPortraitImages(self) -> list:
         images = []
         for i in self.votedUsers:
             images.append(i.portrait_image)
         return images
-    
-    # get users who voted for the specifc party 
+
+    # get users who voted for the specifc party
     def getSpecificPartyUsers(self, party: str) -> list:
         users = []
         for user in self.votedUsers:
@@ -44,15 +90,14 @@ class Voting:
                 users.append(user)
 
         return users
-    
+
     def getWinningParty(self):
         return max(self.partyVotes, key=self.partyVotes.get)
-    
+
     def totalVotes(self):
         return len(self.votedUsers)
-        
 
-    
-    
 
-    
+
+
+
