16a725c97466a3492daaebb718e2c588ff9b0e0d
Yazan Armoush <yazan.armoush1@gmail.com>
2023-04-29 12:31:48 -0400

    Upgraded API Address

--- e-ballot-frontend/src/APIs/FLASK_API.tsx
+++ e-ballot-frontend/src/APIs/FLASK_API.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const FLASK_API_ADDRESS: string = "https://crewiseapi.usec.club";
-const FLASK_LOCAL_ADDRESS: string = "http://127.0.0.1:5000"
+const FLASK_API_ADDRESS: string = "https://e.ballot.api.usec.club";
+const FLASK_LOCAL_ADDRESS: string = "http://127.0.0.1:5050"
 
 const instance = axios.create({
   baseURL: FLASK_API_ADDRESS,
