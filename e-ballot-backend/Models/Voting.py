99aafbcb1cd4762e4ce6c957634a643e897a1145
Yazan Armoush <yazan.armoush1@gmail.com>
2023-04-29 16:33:44 -0400

    Added Party

--- e-ballot-backend/Models/Voting.py
+++ e-ballot-backend/Models/Voting.py
@@ -4,48 +4,7 @@ from bson import ObjectId
 from bson.binary import Binary
 from datetime import datetime
 from routes.users.get import get_users
-from Models import User
-
-class Party:
-    oid: ObjectId
-    name: str
-    ontario: list[ObjectId]
-    quebec: list[ObjectId]
-    bc: list[ObjectId]
-    alberta: list[ObjectId]
-
-    def __init__(self, oid, name, ontario, quebec, bc, alberta):
-        self.oid = oid
-        self.name = name
-        self.ontario = ontario
-        self.quebec = quebec
-        self.bc = bc
-        self.alberta = alberta
-
-    @staticmethod
-    def from_json(doc: dict) -> Party:
-        return User.Profile(
-            oid=doc['_id'],
-            name=doc['name'],
-            ontario=doc['ontario'],
-            quebec=doc['quebec'],
-            bc=doc['bc'],
-            alberta=doc['alberta']
-        )
-
-    def to_json(self) -> dict:
-        return \
-            {
-                '_id': self.oid,
-                'name': self.name,
-                'ontario': self.ontario,
-                'quebec': self.quebec,
-                'bc': self.bc,
-                'alberta': self.alberta
-            }
-
-    def get_all_voters(self):
-        return self.ontario + self.quebec + self.bc + self.alberta
+from Models import User, Party
 
 
 class VotingSystem:
