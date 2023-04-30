672d7554f00629edb8a6b6f779b72dbac073c1d4
Yazan Armoush <yazan.armoush1@gmail.com>
2023-04-30 06:54:04 -0400

    Wrote tests for compare faces

--- e-ballot-backend/tests/compare_faces.py
+++ e-ballot-backend/tests/compare_faces.py
@@ -1,19 +1,71 @@
 from io import BytesIO
 
-from routes.users.post import compare_image
+import imagehash
+import pytest
+from PIL import Image
 
-# test comparing faces of two images
+
+def compare_image(_img1: BytesIO, _img2: BytesIO) -> bool:
+
+    hash0 = imagehash.average_hash(Image.open(_img1))
+    hash1 = imagehash.average_hash(Image.open(_img2))
+    score = 1.0 - ((hash0 - hash1) / 32)
+
+    print("\n========================================")
+    print("The Score: " + str(score))
+    print("========================================\n")
+
+    if score == 1.0:
+        return True
+    elif score >= 0.5:
+        return True
+    else:
+        return False
+
+
+def test_correct():
+
+    with open('tests/data/yazan_id1_1.jpg', 'rb') as f:
+        image_data1 = f.read()
+
+    with open('tests/data/yazan_id1_2.png', 'rb') as f:
+        image_data2 = f.read()
+
+    assert compare_image(BytesIO(image_data1), BytesIO(image_data2))
+
+
+def test_false():
+
+    with open('tests/data/yazan_id1_1.jpg', 'rb') as f:
+        yazan1 = f.read()
+
+    with open('tests/data/ishav_id1_1.jpg', 'rb') as f:
+        ishav1 = f.read()
+
+    with open('tests/data/usman_id1_1.jpg', 'rb') as f:
+        usman1 = f.read()
+
+    with open('tests/data/hussain_id1_1.jpg', 'rb') as f:
+        hussain1 = f.read()
+
+    assert not (compare_image(BytesIO(yazan1), BytesIO(ishav1)))
+    assert not (compare_image(BytesIO(yazan1), BytesIO(usman1)))
+    assert not (compare_image(BytesIO(yazan1), BytesIO(hussain1)))
+
+    assert not (compare_image(BytesIO(ishav1), BytesIO(yazan1)))
+    assert not (compare_image(BytesIO(ishav1), BytesIO(usman1)))
+    assert not (compare_image(BytesIO(ishav1), BytesIO(hussain1)))
+
+    assert not (compare_image(BytesIO(usman1), BytesIO(ishav1)))
+    assert not (compare_image(BytesIO(usman1), BytesIO(yazan1)))
+    assert not (compare_image(BytesIO(usman1), BytesIO(hussain1)))
+
+    assert not (compare_image(BytesIO(hussain1), BytesIO(ishav1)))
+    assert not (compare_image(BytesIO(hussain1), BytesIO(yazan1)))
+    assert not (compare_image(BytesIO(hussain1), BytesIO(usman1)))
+
+
 
 
 if __name__ == '__main__':
-
-    with open('tests/data/yazan_id1.jpg', 'rb') as f:
-        image_data1 = f.read()
-
-    with open('tests/data/yazan_id2.jpg', 'rb') as f:
-        image_data2 = f.read()
-
-    # with open('tests/data/uthman_image.jpg', 'rb') as f:
-    #     image_data3 = f.read()
-
-    print(compare_image(BytesIO(image_data1), BytesIO(image_data2)))
+    pytest.main([])
