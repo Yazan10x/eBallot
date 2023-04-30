c8a5efe9e08353657d6a3f56012610f8aeacf9ee
Usman Tahir <65866570+UsmanTahir7@users.noreply.github.com>
2023-04-30 01:06:51 -0400

    created modal, updated apis,modified userDashboard

--- e-ballot-frontend/src/Components/Users/UserDashboard.tsx
+++ e-ballot-frontend/src/Components/Users/UserDashboard.tsx
@@ -1,186 +1,223 @@
-import * as React from "react"
+import * as React from "react";
 import {
-    Avatar,
-    Box,
-    Center,
-    Flex,
-    Image,
-    Stack,
-    useColorModeValue,
-    VStack,
-    Text,
-    Button,
-    Heading,
-    HStack,
-    Link,
-    Popover,
-    PopoverTrigger,
-    PopoverBody,
-    PopoverCloseButton,
-    PopoverHeader,
-    PopoverContent,
-    PopoverArrow,
-    Spacer,
-    StackDivider,
-    Modal,
-    ModalHeader,
-    ModalOverlay,
-    ModalContent, ModalBody, ModalFooter, ModalCloseButton, useDisclosure
-} from "@chakra-ui/react"
-import {BiExpand, BiParagraph, BiStop, FaGithub, FaLinkedin, FaStop, GrDocumentUpdate, GrUpdate, MdUpdate} from "react-icons/all";
-import {useParams} from "react-router-dom";
-import {UsersAPI} from "../../APIs/UsersAPI";
-import {ObjectID} from "bson";
-import {useEffect, useState} from "react";
-import {User} from "../../Models/User";
-import {ArrowForwardIcon, EditIcon, LockIcon} from "@chakra-ui/icons";
+  Avatar,
+  Box,
+  Center,
+  Flex,
+  Image,
+  Stack,
+  useColorModeValue,
+  VStack,
+  Text,
+  Button,
+  Heading,
+  HStack,
+  Link,
+  Popover,
+  PopoverTrigger,
+  PopoverBody,
+  PopoverCloseButton,
+  PopoverHeader,
+  PopoverContent,
+  PopoverArrow,
+  Spacer,
+  StackDivider,
+  Modal,
+  ModalHeader,
+  ModalOverlay,
+  ModalContent,
+  ModalBody,
+  ModalFooter,
+  ModalCloseButton,
+  useDisclosure,
+  useTagStyles,
+} from "@chakra-ui/react";
+import {
+  BiExpand,
+  BiParagraph,
+  BiStop,
+  FaGithub,
+  FaLinkedin,
+  FaStop,
+  GrDocumentUpdate,
+  GrUpdate,
+  MdUpdate,
+} from "react-icons/all";
+import { useParams } from "react-router-dom";
+import { UsersAPI } from "../../APIs/UsersAPI";
+import { ElectionAPI } from "../../APIs/ElectionAPI";
+import { ObjectID } from "bson";
+import { useEffect, useState } from "react";
+import { User } from "../../Models/User";
+import { Party } from "../../Models/Party";
+import { ArrowForwardIcon, EditIcon, LockIcon } from "@chakra-ui/icons";
 
 export const UserDashboard = () => {
+  let { user_id } = useParams();
+  const [user, setUser] = useState<User>();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [didUserVote, setDidUserVote] = useState<Boolean>(false);
+  const [userPartyVoted, setUserPartyVoted] = useState<Party>();
 
-    let {user_id} = useParams();
-    const [user, setUser] = useState<User>();
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const {
-    isOpen: isOpen2,
-    onOpen: onOpen2,
-    onClose: onClose2
-} = useDisclosure()
+  const get_user = () => {
+    UsersAPI.get_user(ObjectID.createFromHexString(user_id!)).then((res) => {
+      let data = res!;
+      setUser(data);
+    });
+  };
 
-    const get_user = () => {
-        UsersAPI.get_user(ObjectID.createFromHexString(user_id!))
-            .then((res) => {
-                let data = res!
-                setUser(data)
-            })
-    }
+  const get_user_voted_party = (user_id: ObjectID) => {
+    ElectionAPI.did_user_vote(user_id).then((res) => {
+      let data = res!;
+      if (typeof data === 'string') {
+        setDidUserVote(true)
+        setUserPartyVoted(data);
+      } 
+    });
+  };
 
-    const handle_vote = () => {
+  const handle_vote = (party_id: ObjectID) => {
+    ElectionAPI.vote(ObjectID.createFromHexString(user_id!), party_id).then(
+      () => {
+        setDidUserVote(true)
+        onClose();
+      }
+    );
+  };
 
-    }
+  useEffect(() => {
+    get_user();
+    get_user_voted_party(ObjectID.createFromHexString(user_id!))
+  }, []);
 
-    useEffect(() => {
-        get_user();
-    }, [])
+  return (
+    <Center py={6}>
+      <Box
+        w={"full"}
+        bg={useColorModeValue("white", "gray.800")}
+        boxShadow={"2xl"}
+        rounded={"md"}
+        overflow={"hidden"}
+      >
+        <Flex justify={"center"} mt={12}>
+          <Image
+            width={"500px"}
+            src={user?.profile.government_id_image}
+            css={{
+              border: "2px solid white",
+            }}
+          />
+        </Flex>
 
-    return (
-        <Center py={6}>
-            <Box
-                w={'full'}
-                bg={useColorModeValue('white', 'gray.800')}
-                boxShadow={'2xl'}
-                rounded={'md'}
-                overflow={'hidden'}>
-                <Flex justify={'center'} mt={12}>
-                    <Image
-                        width={"500px"}
-                        src={user?.profile.government_id_image}
-                        css={{
-                            border: '2px solid white',
-                        }}
-                    />
-                </Flex>
+        <Box p={6}>
+          <VStack spacing={0} align={"center"} mb={5}>
+            <Heading fontSize={"2xl"} fontWeight={500} fontFamily={"body"}>
+              {user?.profile.first_name + " " + user?.profile.last_name}
+            </Heading>
+            <Text color={"gray.500"}>{user?.profile.email}</Text>
+            <Spacer></Spacer>
+          </VStack>
+          <Center>
+            <HStack align={"stretch"} alignItems={"center"}>
+              <Box>
+                <Heading size="m" textTransform="uppercase">
+                  Date of Birth
+                </Heading>
+                <Text pt="2" fontSize="s">
+                  {user?.profile.dob.toDateString().slice(4)}
+                </Text>
+              </Box>
+              <Spacer width={"80px"}></Spacer>
+              <Box>
+                <Heading size="m" textTransform="uppercase">
+                  Province
+                </Heading>
+                <Text pt="2" fontSize="s">
+                  {user?.province.toUpperCase()}
+                </Text>
+              </Box>
+              <Spacer width={"80px"}></Spacer>
+              <Box>
+                <Heading size="m" textTransform="uppercase">
+                  Gender
+                </Heading>
+                <Text pt="2" fontSize="s">
+                  {user?.profile.gender}
+                </Text>
+              </Box>
+            </HStack>
+          </Center>
+          <Spacer height={"80px"}></Spacer>
+          <Center></Center>
+          <Center>
+            {didUserVote ? (
+              <Button
+                colorScheme="red"
+                bgGradient="linear(to-r, red.400, red.500, red.600)"
+                color="white"
+                variant="solid"
+              >
+                Already Voted for {userPartyVoted!.name}
+              </Button>
+            ) : (
+              <Button
+                colorScheme="red"
+                bgGradient="linear(to-r, red.400, red.500, red.600)"
+                color="white"
+                variant="solid"
+                onClick={onOpen}
+              >
+                Vote!
+              </Button>
+            )}
 
-                <Box p={6}>
-                    <VStack spacing={0} align={'center'} mb={5}>
-                        <Heading fontSize={'2xl'} fontWeight={500} fontFamily={'body'}>
-                            {user?.profile.first_name + " " + user?.profile.last_name}
-                        </Heading>
-                        <Text color={'gray.500'}>{user?.profile.email}</Text>
-                        <Spacer></Spacer>
-                    </VStack>
-                    <Center>
-                        <HStack align={"stretch"} alignItems={'center'}>
-                            <Box>
-                                <Heading size='m' textTransform='uppercase'>
-                                  Date of Birth
-                                </Heading>
-                                <Text pt='2' fontSize='s'>
-                                    {user?.profile.dob.toDateString().slice(4)}
-                                </Text>
-                              </Box>
-                            <Spacer width={"80px"}></Spacer>
-                            <Box>
-                            <Heading size='m' textTransform='uppercase'>
-                              Province
-                            </Heading>
-                            <Text pt='2' fontSize='s'>
-                                {user?.province.toUpperCase()}
-                            </Text>
-                        </Box>
-                            <Spacer width={"80px"}></Spacer>
-                            <Box>
-                                <Heading size='m' textTransform='uppercase'>
-                                  Gender
-                                </Heading>
-                                <Text pt='2' fontSize='s'>
-                                    {user?.profile.gender}
-                                </Text>
-                              </Box>
-                        </HStack>
-                    </Center>
-                    <Spacer height={"80px"}></Spacer>
-                    <Center>
-                    </Center>
-                    <Center>
-                        <Button
-                            colorScheme="red"
-                            bgGradient="linear(to-r, red.400, red.500, red.600)"
-                            color="white"
-                            variant="solid"
-                            onClick={handle_vote}
-                            >
-                            Vote!
-                        </Button>
-                    </Center>
-                    {/*<HStack>*/}
-
-                    {/*    <Button leftIcon={<EditIcon/>} onClick={onOpen2}>*/}
-                    {/*        <Modal isOpen={isOpen2} onClose={onClose2}>*/}
-                    {/*            <ModalOverlay />*/}
-                    {/*            <ModalContent>*/}
-                    {/*              <ModalHeader>Unauthorized Access :: Admin Permission Required</ModalHeader>*/}
-                    {/*              <ModalCloseButton />*/}
-                    {/*                <ModalBody>*/}
-                    {/*                    You need to be an admin to access this page*/}
-                    {/*              </ModalBody>*/}
-                    {/*              <ModalFooter>*/}
-                    {/*                <Button colorScheme='blue' mr={3} onClick={onClose2}>*/}
-                    {/*                  Close*/}
-                    {/*                </Button>*/}
-                    {/*              </ModalFooter>*/}
-                    {/*            </ModalContent>*/}
-                    {/*          </Modal>*/}
-                    {/*    </Button>*/}
-                    {/*    <Button leftIcon={<ArrowForwardIcon/>}>*/}
-                    {/*        <Link href={"/health_report"}>View Medical Record</Link>*/}
-                    {/*    </Button>*/}
-                    {/*    <Spacer></Spacer>*/}
-                    {/*    <Button leftIcon={<BiExpand></BiExpand>} onClick={onOpen}>*/}
-                    {/*        Biography*/}
-                    {/*    </Button>*/}
-                    {/*    <Button leftIcon={<LockIcon></LockIcon>}>*/}
-                    {/*        <Link href={"/incident_report"}>File A Report</Link>*/}
-                    {/*        <Modal isOpen={isOpen} onClose={onClose}>*/}
-                    {/*            <ModalOverlay />*/}
-                    {/*            <ModalContent>*/}
-                    {/*              <ModalHeader>Biography</ModalHeader>*/}
-                    {/*              <ModalCloseButton />*/}
-                    {/*                <ModalBody>*/}
-                    {/*                    {user?.profile.first_name}*/}
-                    {/*              </ModalBody>*/}
-                    {/*              <ModalFooter>*/}
-                    {/*                <Button colorScheme='blue' mr={3} onClick={onClose}>*/}
-                    {/*                  Close*/}
-                    {/*                </Button>*/}
-                    {/*              </ModalFooter>*/}
-                    {/*            </ModalContent>*/}
-                    {/*          </Modal>*/}
-                    {/*    </Button>*/}
-
-
-                    {/*</HStack>*/}
-                </Box>
-            </Box>
-        </Center>
-    )
-    }
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent height={"300px"}>
+                <ModalHeader>Vote Party</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody display="flex" justifyContent="space-around">
+                  <VStack>
+                    <Spacer></Spacer>
+                    <HStack>
+                      <Button
+                        height={20}
+                        width={150}
+                        colorScheme="red"
+                        size={"lg"}
+                        onClick={() =>
+                          handle_vote(
+                            ObjectID.createFromHexString("liberal_party_id")
+                          )
+                        }
+                      >
+                        Liberal
+                      </Button>
+                      <Button
+                        height={20}
+                        width={150}
+                        colorScheme="blue"
+                        size={"lg"}
+                        onClick={() =>
+                          handle_vote(
+                            ObjectID.createFromHexString(
+                              "conservative_party_id"
+                            )
+                          )
+                        }
+                      >
+                        Conservative
+                      </Button>
+                    </HStack>
+                    <Spacer></Spacer>
+                  </VStack>
+                </ModalBody>
+                <ModalFooter></ModalFooter>
+              </ModalContent>
+            </Modal>
+          </Center>
+        </Box>
+      </Box>
+    </Center>
+  );
+};
