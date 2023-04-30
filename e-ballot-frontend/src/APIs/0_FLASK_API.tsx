1af5cbaf736ca519d6634e5db5f2b51fd2ddb786
Usman Tahir <65866570+UsmanTahir7@users.noreply.github.com>
2023-04-30 01:09:03 -0400

    updated frontend ui

--- e-ballot-frontend/src/APIs/0_FLASK_API.tsx
+++ e-ballot-frontend/src/APIs/0_FLASK_API.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const FLASK_API_ADDRESS: string = "https://e.ballot.api.usec.club";
 const FLASK_LOCAL_ADDRESS: string = "http://127.0.0.1:5050";
 
-export const CURRENT_URL = FLASK_LOCAL_ADDRESS
+export const CURRENT_URL = FLASK_API_ADDRESS
 
 export const instance = axios.create({
   baseURL: CURRENT_URL,
